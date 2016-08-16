16657e66d2e28a87140e6150e7a525c7bdf2b7e4
ste <stefan.kosc@gmail.com>
2016-08-16 11:57:23 +0200

    reading contents of directories

---
+++ index.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var events = require('events');
+
+function readEverything(path) {
+
+    fs.readdir(path, function (err, items) {
+        if (!err) {
+            items.forEach(function (item) {
+                fs.stat(path + '/' + item, function (err, stats) {
+                    if (stats.isDirectory()) {
+                        readEverything(path + '/' + item);
+                        fs.readdir(path + '/' + item, function (err, items) {
+                            console.log(path + '/' + item + ' contains ' + items.join(', '));
+                        })
+                    }
+                })
+            })
+        } else {
+            console.log(err);
+            process.exit;
+        }
+    });
+}
+
+var a = readEverything(__dirname);
